8a8cd9076a89118b8ed7d6b6034f9ba1ace68908
SK <englishenglishenglish@proton.me>
2025-06-18 12:53:52 +0000

    Add files via upload

---
+++ 04-Calculating lucas number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+//logic for calculating the lucas nubmer
+int lucas(int n){
+    if(n==0)return 2;
+    if(n==1)return 1;
+
+    int lucas[n+1];
+    lucas[0]=2;
+    lucas[1]=1;
+
+    for(int i=2;i<=n;i++){
+        lucas[i]=lucas[i-1]+lucas[i-2];
+    }
+
+
+    return lucas[n];
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    //logic for special case
+    if(n<0){
+        printf("Invalid input\n");
+        return -1;
+    }
+
+    printf("The %dth lucas number is:%d\n",n,lucas(n));
+
+
+    return 0;
+}
